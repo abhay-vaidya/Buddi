0ec288423a6c33ab7f51431fc893f6220935586c
Abhay Vaidya <abhay6547@gmail.com>
2016-06-05 02:07:20 -0400

    Added functionality to "Change Preferences" button.

--- buddi/src/main/java/group/project/buddi/ProfileFragment.java
+++ buddi/src/main/java/group/project/buddi/ProfileFragment.java
@@ -1,6 +1,7 @@
 package group.project.buddi;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
@@ -9,6 +10,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.gson.JsonArray;
@@ -21,11 +23,24 @@ public class ProfileFragment extends Fragment {
 
     TextView userName;
 
+    Button prefButton;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile,
                 container, false);
+
+        prefButton = (Button) view.findViewById(R.id.changePrefButton);
+
+        prefButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), QuizActivity.class);
+                startActivity(i);
+            }
+        });
 
        // userName = (TextView) view.findViewById(R.id.userName);
 
